5c48c785cf1f962be1f5253e0b004310698682ea
kirran01 <Kirran-@live.com>
2022-12-05 17:06:09 -0500

    completed

--- src/components/ProfileImage.js
+++ src/components/ProfileImage.js
@@ -1,11 +1,11 @@
-import React from 'react';
+import React from "react";
 
-const ProfileImage = () => {
-    return (
-        <div>
-
-        </div>
-    );
-}
+const ProfileImage = (props) => {
+  return (
+    <div>
+      <img src={props.image} className="profile" alt="dog2" />
+    </div>
+  );
+};
 
 export default ProfileImage;
